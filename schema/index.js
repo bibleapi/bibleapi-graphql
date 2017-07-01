2b27f33868ccc37340a7650b4f79be914fdb0ce5
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2017-07-02 00:00:36 +1000

    Sample GraphQL schema

---
+++ schema/index.js
@@ -0,0 +1,16 @@
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        test: {
+            type: GraphQLString,
+            description: 'The sample text in GraphQL',
+            resolve: () => 'Hi GraphQL!'
+        }
+    }
+});
+
+const schema = new GraphQLSchema({ query: RootQueryType });
+
+module.exports = schema;
