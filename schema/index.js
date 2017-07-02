e42328d22e1f8fcb5418ae8209f719dcf69236f2
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2017-07-02 22:31:39 +1000

    Adding Graphiql

--- schema/index.js
+++ schema/index.js
@@ -1,16 +1,16 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
 
 const RootQueryType = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        test: {
-            type: GraphQLString,
-            description: 'The sample text in GraphQL',
-            resolve: () => 'Hi GraphQL!'
-        }
+  name: 'RootQueryType',
+  fields: {
+    test: {
+      type: GraphQLString,
+      description: 'The sample text in GraphQL',
+      resolve: () => 'Hi GraphQL!'
     }
+  }
 });
 
-const schema = new GraphQLSchema({ query: RootQueryType });
+const schema = new GraphQLSchema({query: RootQueryType});
 
 module.exports = schema;
